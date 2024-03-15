237443f8d26d9d97f0e85195931932b66d2a239f
John Stafford <johnstafford@google.com>
2024-03-15 19:09:01 +0000

    [CSS FeedViz] Added class for authenticating service accounts that wish to connect to the CSS API. Based upon https://github.com/googleads/comparison-shopping-service-api-samples/blob/main/java/src/main/java/shopping/css/samples/utils/Authenticator.java
    Note: service-account.json is created explicitly for unit testing and doesn't contain any real credentials.
    
    Change-Id: I01f804eb8f36358109fa5ca9f1dd7b459d01cd75

---
+++ src/main/java/com/google/cssfeedviz/utils/Authenticator.java
@@ -0,0 +1,62 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cssfeedviz.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import com.google.auth.oauth2.GoogleCredentials;
+
+/**
+ * Class that contains all the authentication logic, both for service accounts and to create an
+ * OAuth 2 refresh token for the CSS API.
+ *
+ * <p>IMPORTANT FOR OAUTH: For web app clients types, you must add {@code http://127.0.0.1} to the
+ * "Authorized redirect URIs" list in your Google Cloud Console project before running this example.
+ * Desktop app client types do not require the local redirect to be explicitly configured in the
+ * console.
+ *
+ * <p>This example will start a basic server that listens for requests at {@code
+ * http://127.0.0.1:PORT}, where {@code PORT} is dynamically assigned.
+ */
+public class Authenticator {
+
+  public GoogleCredentials authenticate() throws IOException {
+    return authenticate(AccountInfo.load());
+  }
+  
+  public GoogleCredentials authenticate(AccountInfo accountInfo) throws IOException {
+    // AccountInfo accountInfo = AccountInfo.load();
+    if (accountInfo.getPath() == null) {
+        throw new IllegalArgumentException(
+            "Must update AccountInfo.java to set a configuration directory.");
+    }
+    File serviceAccountFile = new File(accountInfo.getPath(), "service-account.json");
+    System.out.printf("Checking for service account file at: %s%n", serviceAccountFile);
+    if (serviceAccountFile.exists()) {
+      System.out.println("Attempting to load service account credentials");
+      try (InputStream inputStream = new FileInputStream(serviceAccountFile)) {
+        GoogleCredentials credential = GoogleCredentials.fromStream(inputStream);
+        System.out.println("Successfully loaded service account credentials");
+        return credential;
+      }
+    } else {
+      throw new IOException(
+          "Could not retrieve service account credentials from the file "
+              + serviceAccountFile.getCanonicalPath());
+    }
+  }
+}
---
+++ src/test/java/com/google/cssfeedviz/utils/AuthenticatorTest.java
@@ -0,0 +1,75 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cssfeedviz.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.UserAuthorizer;
+import com.google.auth.oauth2.UserCredentials;
+
+public class AuthenticatorTest {
+  
+  private final String TEST_CONFIG_DIR = "./config/test";
+  private final String INVALID_TEST_CONFIG_DIR = TEST_CONFIG_DIR + "/invalid";
+  private final String SERVICE_ACCOUNT_FILE_PATH = TEST_CONFIG_DIR + "/service-account.json";
+
+  @Mock
+  private AccountInfo accountInfo;
+
+  @InjectMocks
+  private Authenticator authenticator;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testAuthenticate_serviceAccount() throws IOException {
+    when(accountInfo.getPath()).thenReturn(new File(TEST_CONFIG_DIR));
+
+    GoogleCredentials expectedCredentials =
+      GoogleCredentials.fromStream(new FileInputStream(SERVICE_ACCOUNT_FILE_PATH));
+    GoogleCredentials credentials = authenticator.authenticate(accountInfo);
+
+    assertEquals(expectedCredentials, credentials);
+  }
+
+  @Test
+  public void testAuthenticate_serviceAccount_noPath() throws IOException {
+    when(accountInfo.getPath()).thenReturn(null);
+
+    assertThrows(IllegalArgumentException.class, () -> authenticator.authenticate(accountInfo));
+  }
+
+  @Test
+  public void testAuthenticate_serviceAccount_fileNotFound() throws IOException {
+    when(accountInfo.getPath()).thenReturn(new File(INVALID_TEST_CONFIG_DIR));
+
+    assertThrows(IOException.class, () -> authenticator.authenticate(accountInfo));
+  }
+}
