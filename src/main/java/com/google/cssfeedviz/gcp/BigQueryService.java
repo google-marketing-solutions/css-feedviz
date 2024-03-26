c94260a0277aaa68d1d7a8e9b60d845afbac00c3
John Stafford <johnstafford@google.com>
2024-03-26 12:17:35 +0000

    [CSS FeedViz] Added functionality for retrieving Schema for Css Products BQ Table. Based upon CSS API documentation at https://developers.devsite.corp.google.com/comparison-shopping-services/api/reference/rpc/google.shopping.css.v1#cssproduct
    Change-Id: Ia84a32e52f33750c38836ca4d914019065426c3d

--- src/main/java/com/google/cssfeedviz/gcp/BigQueryService.java
+++ src/main/java/com/google/cssfeedviz/gcp/BigQueryService.java
@@ -20,6 +20,10 @@ import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.bigquery.Dataset;
 import com.google.cloud.bigquery.DatasetId;
 import com.google.cloud.bigquery.DatasetInfo;
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.Field.Mode;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardSQLTypeName;
 import com.google.cssfeedviz.utils.AccountInfo;
 import com.google.cssfeedviz.utils.Authenticator;
 import java.io.IOException;
@@ -39,6 +43,163 @@ public class BigQueryService {
   public Dataset createDataset(String datasetName, String location) {
     DatasetInfo datasetInfo = DatasetInfo.newBuilder(datasetName).setLocation(location).build();
     return this.bigQuery.create(datasetInfo);
+  }
+
+  public Field getCssProductsAttributesField() {
+    Field priceAmountMicros = Field.of("amount_micros", StandardSQLTypeName.INT64);
+    Field priceCurrencyCode = Field.of("currency_code", StandardSQLTypeName.STRING);
+    Field productDimensionValue = Field.of("value", StandardSQLTypeName.FLOAT64);
+    Field productDimensionUnit = Field.of("unit", StandardSQLTypeName.STRING);
+    return Field.of(
+        "attributes",
+        StandardSQLTypeName.STRUCT,
+        Field.of("low_price", StandardSQLTypeName.STRUCT, priceAmountMicros, priceCurrencyCode),
+        Field.of("high_price", StandardSQLTypeName.STRUCT, priceAmountMicros, priceCurrencyCode),
+        Field.of(
+            "headline_offer_price",
+            StandardSQLTypeName.STRUCT,
+            priceAmountMicros,
+            priceCurrencyCode),
+        Field.of(
+            "headline_offer_shipping_price",
+            StandardSQLTypeName.STRUCT,
+            priceAmountMicros,
+            priceCurrencyCode),
+        Field.newBuilder("additional_image_links", StandardSQLTypeName.STRING)
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.newBuilder("product_types", StandardSQLTypeName.STRING)
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.newBuilder("size_types", StandardSQLTypeName.STRING).setMode(Mode.REPEATED).build(),
+        Field.newBuilder(
+                "product_details",
+                StandardSQLTypeName.STRUCT,
+                Field.of("section_name", StandardSQLTypeName.STRING),
+                Field.of("attribute_name", StandardSQLTypeName.STRING),
+                Field.of("attribute_value", StandardSQLTypeName.STRING))
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.of(
+            "product_weight",
+            StandardSQLTypeName.STRUCT,
+            productDimensionValue,
+            productDimensionUnit),
+        Field.of(
+            "product_length",
+            StandardSQLTypeName.STRUCT,
+            productDimensionValue,
+            productDimensionUnit),
+        Field.of(
+            "product_width",
+            StandardSQLTypeName.STRUCT,
+            productDimensionValue,
+            productDimensionUnit),
+        Field.of(
+            "product_height",
+            StandardSQLTypeName.STRUCT,
+            productDimensionValue,
+            productDimensionUnit),
+        Field.newBuilder("product_highlights", StandardSQLTypeName.STRING)
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.newBuilder(
+                "certifications",
+                StandardSQLTypeName.STRUCT,
+                Field.of("name", StandardSQLTypeName.STRING),
+                Field.of("authority", StandardSQLTypeName.STRING),
+                Field.of("code", StandardSQLTypeName.STRING))
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.of("expiration_date", StandardSQLTypeName.TIMESTAMP),
+        Field.newBuilder("included_destinations", StandardSQLTypeName.STRING)
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.newBuilder("excluded_destinations", StandardSQLTypeName.STRING)
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.of("cpp_link", StandardSQLTypeName.STRING),
+        Field.of("cpp_mobile_link", StandardSQLTypeName.STRING),
+        Field.of("cpp_ads_redirect", StandardSQLTypeName.STRING),
+        Field.of("number_of_offers", StandardSQLTypeName.INT64),
+        Field.of("headline_offer_condition", StandardSQLTypeName.STRING),
+        Field.of("headline_offer_link", StandardSQLTypeName.STRING),
+        Field.of("headline_offer_mobile_link", StandardSQLTypeName.STRING),
+        Field.of("title", StandardSQLTypeName.STRING),
+        Field.of("image_link", StandardSQLTypeName.STRING),
+        Field.of("description", StandardSQLTypeName.STRING),
+        Field.of("brand", StandardSQLTypeName.STRING),
+        Field.of("mpn", StandardSQLTypeName.STRING),
+        Field.of("gtin", StandardSQLTypeName.STRING),
+        Field.of("google_product_category", StandardSQLTypeName.STRING),
+        Field.of("adult", StandardSQLTypeName.BOOL),
+        Field.of("multipack", StandardSQLTypeName.INT64),
+        Field.of("is_bundle", StandardSQLTypeName.BOOL),
+        Field.of("age_group", StandardSQLTypeName.STRING),
+        Field.of("color", StandardSQLTypeName.STRING),
+        Field.of("gender", StandardSQLTypeName.STRING),
+        Field.of("material", StandardSQLTypeName.STRING),
+        Field.of("pattern", StandardSQLTypeName.STRING),
+        Field.of("size", StandardSQLTypeName.STRING),
+        Field.of("size_system", StandardSQLTypeName.STRING),
+        Field.of("item_group_id", StandardSQLTypeName.STRING),
+        Field.of("pause", StandardSQLTypeName.STRING),
+        Field.of("custom_label_0", StandardSQLTypeName.STRING),
+        Field.of("custom_label_1", StandardSQLTypeName.STRING),
+        Field.of("custom_label_2", StandardSQLTypeName.STRING),
+        Field.of("custom_label_3", StandardSQLTypeName.STRING),
+        Field.of("custom_label_4", StandardSQLTypeName.STRING));
+  }
+
+  public Field getCssProductsCssProductStatusField() {
+    return Field.of(
+        "css_product_status",
+        StandardSQLTypeName.STRUCT,
+        Field.newBuilder(
+                "destination_statuses",
+                StandardSQLTypeName.STRUCT,
+                Field.of("destination", StandardSQLTypeName.STRING),
+                Field.newBuilder("approved_countries", StandardSQLTypeName.STRING)
+                    .setMode(Mode.REPEATED)
+                    .build(),
+                Field.newBuilder("pending_countries", StandardSQLTypeName.STRING)
+                    .setMode(Mode.REPEATED)
+                    .build(),
+                Field.newBuilder("disapproved_countries", StandardSQLTypeName.STRING)
+                    .setMode(Mode.REPEATED)
+                    .build())
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.newBuilder(
+                "item_level_issues",
+                StandardSQLTypeName.STRUCT,
+                Field.of("code", StandardSQLTypeName.STRING),
+                Field.of("servability", StandardSQLTypeName.STRING),
+                Field.of("resolution", StandardSQLTypeName.STRING),
+                Field.of("attribute", StandardSQLTypeName.STRING),
+                Field.of("destination", StandardSQLTypeName.STRING),
+                Field.of("description", StandardSQLTypeName.STRING),
+                Field.of("detail", StandardSQLTypeName.STRING),
+                Field.of("documentation", StandardSQLTypeName.STRING),
+                Field.newBuilder("applicable_countries", StandardSQLTypeName.STRING)
+                    .setMode(Mode.REPEATED)
+                    .build())
+            .setMode(Mode.REPEATED)
+            .build(),
+        Field.of("creation_date", StandardSQLTypeName.TIMESTAMP),
+        Field.of("last_update_date", StandardSQLTypeName.TIMESTAMP),
+        Field.of("google_expiration_date", StandardSQLTypeName.TIMESTAMP));
+  }
+
+  public Schema getCssProductsSchema() {
+    return Schema.of(
+        Field.of("date", StandardSQLTypeName.DATE),
+        Field.of("name", StandardSQLTypeName.STRING),
+        Field.of("raw_provided_id", StandardSQLTypeName.STRING),
+        Field.of("content_language", StandardSQLTypeName.STRING),
+        Field.of("feed_label", StandardSQLTypeName.STRING),
+        getCssProductsAttributesField(),
+        getCssProductsCssProductStatusField());
   }
 
   public BigQueryService(AccountInfo accountInfo) throws IOException {
--- src/test/java/com/google/cssfeedviz/gcp/BigQueryServiceTest.java
+++ src/test/java/com/google/cssfeedviz/gcp/BigQueryServiceTest.java
@@ -23,6 +23,10 @@ import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.Dataset;
 import com.google.cloud.bigquery.DatasetId;
 import com.google.cloud.bigquery.DatasetInfo;
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.Field.Mode;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardSQLTypeName;
 import com.google.cssfeedviz.utils.AccountInfo;
 import java.io.IOException;
 import org.junit.Before;
@@ -39,6 +43,150 @@ public class BigQueryServiceTest {
   private final DatasetId DATASET_ID = DatasetId.of(TEST_DATASET_NAME);
   private final DatasetInfo DATASET_INFO =
       DatasetInfo.newBuilder(TEST_DATASET_NAME).setLocation(TEST_LOCATION).build();
+  private final Field PRICE_AMOUNT_MICROS = Field.of("amount_micros", StandardSQLTypeName.INT64);
+  private final Field PRICE_CURRENCY_CODE = Field.of("currency_code", StandardSQLTypeName.STRING);
+  private final Field PRODUCT_DIMENSION_VALUE = Field.of("value", StandardSQLTypeName.FLOAT64);
+  private final Field PRODUCT_DIMENSION_UNIT = Field.of("unit", StandardSQLTypeName.STRING);
+  private final Field CSS_PRODUCTS_ATTRIBUTES_FIELD =
+      Field.of(
+          "attributes",
+          StandardSQLTypeName.STRUCT,
+          Field.of(
+              "low_price", StandardSQLTypeName.STRUCT, PRICE_AMOUNT_MICROS, PRICE_CURRENCY_CODE),
+          Field.of(
+              "high_price", StandardSQLTypeName.STRUCT, PRICE_AMOUNT_MICROS, PRICE_CURRENCY_CODE),
+          Field.of(
+              "headline_offer_price",
+              StandardSQLTypeName.STRUCT,
+              PRICE_AMOUNT_MICROS,
+              PRICE_CURRENCY_CODE),
+          Field.of(
+              "headline_offer_shipping_price",
+              StandardSQLTypeName.STRUCT,
+              PRICE_AMOUNT_MICROS,
+              PRICE_CURRENCY_CODE),
+          Field.newBuilder("additional_image_links", StandardSQLTypeName.STRING)
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.newBuilder("product_types", StandardSQLTypeName.STRING)
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.newBuilder("size_types", StandardSQLTypeName.STRING).setMode(Mode.REPEATED).build(),
+          Field.newBuilder(
+                  "product_details",
+                  StandardSQLTypeName.STRUCT,
+                  Field.of("section_name", StandardSQLTypeName.STRING),
+                  Field.of("attribute_name", StandardSQLTypeName.STRING),
+                  Field.of("attribute_value", StandardSQLTypeName.STRING))
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.of(
+              "product_weight",
+              StandardSQLTypeName.STRUCT,
+              PRODUCT_DIMENSION_VALUE,
+              PRODUCT_DIMENSION_UNIT),
+          Field.of(
+              "product_length",
+              StandardSQLTypeName.STRUCT,
+              PRODUCT_DIMENSION_VALUE,
+              PRODUCT_DIMENSION_UNIT),
+          Field.of(
+              "product_width",
+              StandardSQLTypeName.STRUCT,
+              PRODUCT_DIMENSION_VALUE,
+              PRODUCT_DIMENSION_UNIT),
+          Field.of(
+              "product_height",
+              StandardSQLTypeName.STRUCT,
+              PRODUCT_DIMENSION_VALUE,
+              PRODUCT_DIMENSION_UNIT),
+          Field.newBuilder("product_highlights", StandardSQLTypeName.STRING)
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.newBuilder(
+                  "certifications",
+                  StandardSQLTypeName.STRUCT,
+                  Field.of("name", StandardSQLTypeName.STRING),
+                  Field.of("authority", StandardSQLTypeName.STRING),
+                  Field.of("code", StandardSQLTypeName.STRING))
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.of("expiration_date", StandardSQLTypeName.TIMESTAMP),
+          Field.newBuilder("included_destinations", StandardSQLTypeName.STRING)
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.newBuilder("excluded_destinations", StandardSQLTypeName.STRING)
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.of("cpp_link", StandardSQLTypeName.STRING),
+          Field.of("cpp_mobile_link", StandardSQLTypeName.STRING),
+          Field.of("cpp_ads_redirect", StandardSQLTypeName.STRING),
+          Field.of("number_of_offers", StandardSQLTypeName.INT64),
+          Field.of("headline_offer_condition", StandardSQLTypeName.STRING),
+          Field.of("headline_offer_link", StandardSQLTypeName.STRING),
+          Field.of("headline_offer_mobile_link", StandardSQLTypeName.STRING),
+          Field.of("title", StandardSQLTypeName.STRING),
+          Field.of("image_link", StandardSQLTypeName.STRING),
+          Field.of("description", StandardSQLTypeName.STRING),
+          Field.of("brand", StandardSQLTypeName.STRING),
+          Field.of("mpn", StandardSQLTypeName.STRING),
+          Field.of("gtin", StandardSQLTypeName.STRING),
+          Field.of("google_product_category", StandardSQLTypeName.STRING),
+          Field.of("adult", StandardSQLTypeName.BOOL),
+          Field.of("multipack", StandardSQLTypeName.INT64),
+          Field.of("is_bundle", StandardSQLTypeName.BOOL),
+          Field.of("age_group", StandardSQLTypeName.STRING),
+          Field.of("color", StandardSQLTypeName.STRING),
+          Field.of("gender", StandardSQLTypeName.STRING),
+          Field.of("material", StandardSQLTypeName.STRING),
+          Field.of("pattern", StandardSQLTypeName.STRING),
+          Field.of("size", StandardSQLTypeName.STRING),
+          Field.of("size_system", StandardSQLTypeName.STRING),
+          Field.of("item_group_id", StandardSQLTypeName.STRING),
+          Field.of("pause", StandardSQLTypeName.STRING),
+          Field.of("custom_label_0", StandardSQLTypeName.STRING),
+          Field.of("custom_label_1", StandardSQLTypeName.STRING),
+          Field.of("custom_label_2", StandardSQLTypeName.STRING),
+          Field.of("custom_label_3", StandardSQLTypeName.STRING),
+          Field.of("custom_label_4", StandardSQLTypeName.STRING));
+  private final Field CSS_PRODUCTS_CSS_PRODUCT_STATUS_FIELD =
+      Field.of(
+          "css_product_status",
+          StandardSQLTypeName.STRUCT,
+          Field.newBuilder(
+                  "destination_statuses",
+                  StandardSQLTypeName.STRUCT,
+                  Field.of("destination", StandardSQLTypeName.STRING),
+                  Field.newBuilder("approved_countries", StandardSQLTypeName.STRING)
+                      .setMode(Mode.REPEATED)
+                      .build(),
+                  Field.newBuilder("pending_countries", StandardSQLTypeName.STRING)
+                      .setMode(Mode.REPEATED)
+                      .build(),
+                  Field.newBuilder("disapproved_countries", StandardSQLTypeName.STRING)
+                      .setMode(Mode.REPEATED)
+                      .build())
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.newBuilder(
+                  "item_level_issues",
+                  StandardSQLTypeName.STRUCT,
+                  Field.of("code", StandardSQLTypeName.STRING),
+                  Field.of("servability", StandardSQLTypeName.STRING),
+                  Field.of("resolution", StandardSQLTypeName.STRING),
+                  Field.of("attribute", StandardSQLTypeName.STRING),
+                  Field.of("destination", StandardSQLTypeName.STRING),
+                  Field.of("description", StandardSQLTypeName.STRING),
+                  Field.of("detail", StandardSQLTypeName.STRING),
+                  Field.of("documentation", StandardSQLTypeName.STRING),
+                  Field.newBuilder("applicable_countries", StandardSQLTypeName.STRING)
+                      .setMode(Mode.REPEATED)
+                      .build())
+              .setMode(Mode.REPEATED)
+              .build(),
+          Field.of("creation_date", StandardSQLTypeName.TIMESTAMP),
+          Field.of("last_update_date", StandardSQLTypeName.TIMESTAMP),
+          Field.of("google_expiration_date", StandardSQLTypeName.TIMESTAMP));
 
   private AccountInfo accountInfo;
   private BigQueryService bigQueryService;
@@ -70,5 +218,32 @@ public class BigQueryServiceTest {
   public void createDataset() throws IOException {
     when(bigQuery.create(DATASET_INFO)).thenReturn(dataset);
     assertEquals(dataset, bigQueryService.createDataset(TEST_DATASET_NAME, TEST_LOCATION));
+  }
+
+  @Test
+  public void getCssProductsAttributesField() {
+    assertEquals(CSS_PRODUCTS_ATTRIBUTES_FIELD, bigQueryService.getCssProductsAttributesField());
+  }
+
+  @Test
+  public void getCssProductsCssProductStatusField() {
+    assertEquals(
+        CSS_PRODUCTS_CSS_PRODUCT_STATUS_FIELD,
+        bigQueryService.getCssProductsCssProductStatusField());
+  }
+
+  @Test
+  public void getProductsSchema() {
+    Schema schema =
+        Schema.of(
+            Field.of("date", StandardSQLTypeName.DATE),
+            Field.of("name", StandardSQLTypeName.STRING),
+            Field.of("raw_provided_id", StandardSQLTypeName.STRING),
+            Field.of("content_language", StandardSQLTypeName.STRING),
+            Field.of("feed_label", StandardSQLTypeName.STRING),
+            CSS_PRODUCTS_ATTRIBUTES_FIELD,
+            CSS_PRODUCTS_CSS_PRODUCT_STATUS_FIELD);
+
+    assertEquals(schema, bigQueryService.getCssProductsSchema());
   }
 }
