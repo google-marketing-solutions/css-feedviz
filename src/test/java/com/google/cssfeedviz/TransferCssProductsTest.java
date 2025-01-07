1f060c08314dee2eb2659bd0f6279fef4ef35928
John Stafford <johnstafford@google.com>
2025-01-07 10:50:16 +0000

    [CSS FeedViz] Added test to TransferCssProductsTest.java to see if behavious is consistent when CSS Center account information is specified using System Properties instead of a config JSON file
    Change-Id: I61e5edc4675c08d7d0ab5cd9b6c55444089b7e38

--- src/test/java/com/google/cssfeedviz/TransferCssProductsTest.java
+++ src/test/java/com/google/cssfeedviz/TransferCssProductsTest.java
@@ -24,6 +24,9 @@ public class TransferCssProductsTest {
   private final String TEST_CONFIG_DIR = "./config/test";
   private final String TEST_DATASET_NAME = "css_feedviz";
   private final String TEST_DATASET_LOCATION = "EU";
+  private final String TEST_GROUP_ID = "123";
+  private final String TEST_DOMAIN_ID = "456";
+  private final String TEST_MERCHANT_ID = "789";
   private final CssProduct CSS_PRODUCT = CssProduct.newBuilder().setName(TEST_PRODUCT_NAME).build();
   private final List<CssProduct> CSS_PRODUCT_LIST = List.of(CSS_PRODUCT);
   private final LocalDateTime TEST_TRANSFER_DATE = LocalDateTime.now();
@@ -62,6 +65,27 @@ public class TransferCssProductsTest {
           IOException,
           IllegalArgumentException,
           DescriptorValidationException {
+    TransferCssProducts.main(null);
+    ProductsService mockProductsService = mockProductsServiceController.constructed().get(0);
+    verify(mockProductsService).listCssProducts();
+
+    BigQueryService mockBigQueryService = mockBigQueryServiceController.constructed().get(0);
+    verify(mockBigQueryService)
+        .streamCssProducts(
+            TEST_DATASET_NAME, TEST_DATASET_LOCATION, CSS_PRODUCT_LIST, TEST_TRANSFER_DATE);
+  }
+
+  @Test
+  public void testMain_withAccountSystemPropertiesSet()
+      throws ExecutionException,
+          InterruptedException,
+          IOException,
+          IllegalArgumentException,
+          DescriptorValidationException {
+    System.setProperty("feedviz.account.info.domain.id", TEST_DOMAIN_ID);
+    System.setProperty("feedviz.account.info.group.id", TEST_GROUP_ID);
+    System.setProperty("feedviz.account.info.merchant.id", TEST_MERCHANT_ID);
+
     TransferCssProducts.main(null);
     ProductsService mockProductsService = mockProductsServiceController.constructed().get(0);
     verify(mockProductsService).listCssProducts();
